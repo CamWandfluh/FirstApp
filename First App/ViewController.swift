e59ed25091b27c3f83d11d06b81df73c057eab9c
Cameron Wandfluh <home@mwc-013088.dhcp.missouri.edu>
2017-08-24 13:09:06 -0500

    Created Initial Project

---
+++ First App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First App
+//
+//  Created by Cameron Wandfluh on 8/24/17.
+//  Copyright © 2017 Cameron Wandfluh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
